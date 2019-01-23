61362a2661511f67fa9bee572f807b23ced82f3f
fanpei <fanpei@mycompany.com>
2019-01-23 08:36:09 +0000

    1.修改jar包引用获取运行根目录 2.修复配置文件不存在，默认配置文件复制到目标路径【为解决jar包引用无法打开配置文件问题】 3.pom文件新增生成源代码插件配置

--- src/main/java/soft/net/conf/ConfReader.java
+++ src/main/java/soft/net/conf/ConfReader.java
@@ -2,12 +2,15 @@ package soft.net.conf;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URLDecoder;
+import java.io.InputStream;
+
+import org.apache.commons.io.FileUtils;
 
 import soft.common.conf.ConfException;
 import soft.common.exception.DataIsNullException;
 import soft.common.exception.OstypeMissWatchException;
 import soft.common.file.PathUtil;
+import soft.common.log.Log4j2Writer;
 import soft.common.system.AppRunPathUitl;
 
 public class ConfReader {
@@ -30,7 +33,8 @@ public class ConfReader {
 		File confile = new File(confPath);
 		if (!confile.exists()) {// 不存在使用默认配置
 			System.err.println(CONF + " is not found,sys will use default config");
-			confPath = URLDecoder.decode(ConfReader.class.getClassLoader().getResource(CONF).getPath(), "utf-8");
+			InputStream in = Log4j2Writer.class.getClassLoader().getResourceAsStream(CONF);
+			FileUtils.copyInputStreamToFile(in, confile);
 		}
 
 	}
